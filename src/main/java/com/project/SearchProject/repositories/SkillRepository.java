722c5c6e06036c88137b2ed3198d9f7af82d9122
cmtp <christian.tola@hotmail.com>
2017-06-11 14:56:42 -0400

    Add SkillRepository

---
+++ src/main/java/com/project/SearchProject/repositories/SkillRepository.java
@@ -0,0 +1,10 @@
+package com.project.SearchProject.repositories;
+
+import com.project.SearchProject.model.Skill;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author ctola
+ */
+public interface SkillRepository extends CrudRepository<Skill, Long> {
+}
